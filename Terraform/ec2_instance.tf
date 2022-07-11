968e00f5e7347b1545707e5ab962a286644bb358
aondwar <109013134+aondwar@users.noreply.github.com>
2022-07-11 10:09:16 +0530

    Update ec2_instance.tf

--- Terraform/ec2_instance.tf
+++ Terraform/ec2_instance.tf
@@ -1,5 +1,6 @@
 # ec2 instance
 resource "aws_instance" "ec2-instance" {
+  region                  = var.region
   ami                     = var.ami_id
   instance_type           = var.instance_type
   disable_api_stop        = true
